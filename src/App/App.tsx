108b6b5a24eb24f691dfb67f14e7f6ce2ef92d95
CJL Fortunato <clemence.fortunato@gmail.com>
2021-12-21 17:54:44 +0100

    Set up project

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './reset.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Profile Utilisateur</h1>
+    </div>
+  );
+}
+
+export default App;
