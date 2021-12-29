1f4e9ca25ef8d1c811b0cb3eb86151251bcbb000
CJL Fortunato <clemence.fortunato@gmail.com>
2021-12-29 10:50:33 +0100

    Add more setup for unit tests

---
+++ src/Components/SaveButton/SaveButton.test.js
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import { SaveButton } from './SaveButton';
+import React from "react";
+
+/**
+ * @jest-environment jsdom
+ */
+
+describe("Save Button", () => {
+
+    it("renders on the page", () => {
+        const { getByText } = render(<SaveButton/>);
+        const saveButton = getByText("Save");
+
+        expect(saveButton).toBeTruthy();
+    });
+
+});
---
+++ src/Components/SavedTopics/SavedTopics.test.js
@@ -0,0 +1,4 @@
+import { render } from '@testing-library/react';
+import { SavedTopics } from './SavedTopics';
+import React from "react";
+
